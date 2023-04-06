54ac02026f0571995b32246945528778a5767a4b
msahmed646 <msahmed646@gmail.com>
2023-04-06 14:00:25 -0400

    Apr 6 commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,11 @@
 $(function() {
-    $('.dropdown-toggle').dropdown()
-
-
-
-    // $(".carousel").carousel( { interval: 2000, pause: "false" } );
-    // $("#carouselButton").click(function(){
-    //     if($("#carouselButton").children("i").hasClass("fa-pause")){
-    //         $(".carousel").carousel("pause");
-    //         $("#carouselButton").children("i").removeClass("fa-pause");
-    //         $("#carouselButton").children("i").addClass("fa-play");
-    //     } else{
-    //         $(".carousel").carousel("cycle");
-    //         $("#carouselButton").children("i").removeClass("fa-play");
-    //         $("#carouselButton").children("i").addClass("fa-pause");
-    //     }
-    // });
-    // $("#reserveButton").click (function() {
-    //     $("#reserveModal").modal('show');
-    // });
-
-    // $("#loginButton").click(function() {
-    //     $("#loginModal").modal('show');
-    //   });
+      $('#exampleModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('whatever') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+        modal.find('.modal-body input').val(recipient)
+      })
 });
