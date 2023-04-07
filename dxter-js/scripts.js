7eaa11f42098a430da656036a3e373b310d51576
msahmed646 <msahmed646@gmail.com>
2023-04-07 01:13:21 -0400

    Add messages and Settings Pages

--- dxter-js/scripts.js
+++ dxter-js/scripts.js
@@ -1,19 +1,18 @@
-// $(function() {
-//       $('#exampleModal').on('show.bs.modal', function (event) {
-//         var button = $(event.relatedTarget) // Button that triggered the modal
-//         var recipient = button.data('whatever') // Extract info from data-* attributes
-//         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-//         // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-//         var modal = $(this)
-//         modal.find('.modal-title').text('New message to ' + recipient)
-//         modal.find('.modal-body input').val(recipient)
-//       })
+$(function() {
+      $('#hkAccordion').on('hidden.bs.collapse', function () {
+        show.bs.collapse
+      })
+      
+});
 
-//       $('#hkAccordion').on('hidden.bs.collapse', function () {
-//         show.bs.collapse
-//       }) 
-// });
+function showGoAlert() {
+    alert("Sorry... this function has not been perfected yet!");
+}
 
-$('#hkAccordion').on('hidden.bs.collapse', function () {
-    show.bs.collapse
-})
+function logoutAlert() {
+    alert("This will lead you back to the login page, that has not been designed yet.");
+}
+
+
+
+
